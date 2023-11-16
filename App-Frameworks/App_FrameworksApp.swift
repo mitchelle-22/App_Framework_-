359902055a07c64610a84a39775423c22884e0a2
Mich <mitchellefetty@gmail.com>
2023-11-16 08:18:51 +0200

    Initial Commit

---
+++ App-Frameworks/App_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_FrameworksApp.swift
+//  App-Frameworks
+//
+//  Created by MIch on 2023/11/16.
+//
+
+import SwiftUI
+
+@main
+struct App_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
